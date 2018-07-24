070c84aa2efdc7c932cdd6fd51c56a5aa2dbbcf7
alexis.gayte <alexis.gayte@gmail.com>
2018-07-24 11:37:50 +0100

    add test

--- src/main/java/uk/gov/hmcts/reform/ref/pup/batch/config/ScheduleConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/ref/pup/batch/config/ScheduleConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
@@ -15,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
-@EnableBatchProcessing
 @ConditionalOnProperty("toggle.uploadCSV")
 public class ScheduleConfiguration {
 
