7599b3f3dcdf9c6607a4d016cf4878077f028118
alexis.gayte <alexis.gayte@gmail.com>
2018-08-16 12:08:01 +0100

    tidy up the build.

--- src/main/java/uk/gov/hmcts/reform/ref/pup/Application.java
+++ src/main/java/uk/gov/hmcts/reform/ref/pup/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("HideUtilityClassConstructor")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
